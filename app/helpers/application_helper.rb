ddb41277472a240410e673b0a49f50e50dacdd3e
ngodi <albertngodi@gmail.com>
2021-07-19 13:11:51 +0100

    setup user avatar with gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
-end
+    def avatar_url(user, size)
+      gravatar_id = Digest::MD5.hexdigest(current_user.email.downcase)
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+    end
+  end
